412b69b5313c93ad21c6e28d8e8dde534586225e
Christopher-Emunah-Kim <hashin.chris@gmail.com>
2025-11-28 14:08:22 +0900

    <feat> add default classes and bp

---
+++ Source/Nekiro/Public/Player/K_MainPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "K_MainPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEKIRO_API AK_MainPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
